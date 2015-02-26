f435f6681d102b91e364cf3924aa9198526872c8
vo-lupt-as <vo-lupt-as@users.noreply.github.com>
2015-02-26 19:56:23 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+//sw.js
+this.addEventListener('install', function(e) {
+e.waitUntil(
+    console.log(e)
+    )
+});
+
+this.addEventListener('activate', function(e) {
+    console.log(e)
+});
+
+this.addEventListener('error', function(e) {
+    console.error(e)
+});
+
+this.addEventListener('fetch', function(e) {
+    console.error(e)
+});
